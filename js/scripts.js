3a8bb83a8f7a26bcae9a33185f8acbc0e35efeb8
care0hlyn <ckhuynh20@gmail.com>
2017-02-09 10:33:22 -0800

    scout ui

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+	$(".onboarding-1").hide();
+    $(".onboarding-2").hide();
+    $(".onboarding-3").hide();
+    $(".onboarding-4").hide();
+    $(".loading").show();
+
+    $(".loading").on( "click", function() {
+        $(".onboarding-1").show();
+        $(this).hide();
+    });
+
+    $(".get-started-btn").on("click", function() {
+        $(".onboarding-2").show();
+        $(".onboarding-1").hide();
+    });
+
+    $(".gmail-btn").on("click", function() {
+        $(".onboarding-3").show();
+        $(".onboarding-2").hide();
+    });
+
+    $(".fb-btn").on("click", function() {
+        $(".onboarding-3").show();
+        $(".onboarding-2").hide();
+    });
+
+    $(".pbar-2").on("click", function() {
+        $(".onboarding-3").hide();
+        $(".onboarding-4").show();
+    });
+
+});
